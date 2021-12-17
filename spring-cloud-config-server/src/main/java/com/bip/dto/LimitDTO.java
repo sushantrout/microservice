eccb537eb2e228946c32047cb03c9aa50037778d
Sushant Kumar Rout <38247082+sushantrout@users.noreply.github.com>
2021-12-17 16:11:48 +0530

    initial commit

--- spring-cloud-config-server/src/main/java/com/bip/dto/LimitDTO.java
+++ spring-cloud-config-server/src/main/java/com/bip/dto/LimitDTO.java
@@ -1,11 +1,13 @@
 package com.bip.dto;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
 
 import lombok.Data;
 
 @Data
 @ConfigurationProperties("limit-service")
+@Component
 public class LimitDTO {
 	Integer max;
 	Integer min;
