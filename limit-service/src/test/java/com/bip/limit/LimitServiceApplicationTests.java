f9c9327ab17cfab51ad998eaef37893275b6a2eb
Sushant Kumar Rout <38247082+sushantrout@users.noreply.github.com>
2021-12-17 15:47:35 +0530

    initial commit

---
+++ limit-service/src/test/java/com/bip/limit/LimitServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bip.limit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
